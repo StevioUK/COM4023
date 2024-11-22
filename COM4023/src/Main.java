ab0637d6151d1a4dd3fa694a2eec2ea67b826f79
Stevio <59091187+StevioUK@users.noreply.github.com>
2024-11-22 12:25:54 +0000

    Working on storing classes in array
    Added a way to store classes into the arrayList

--- COM4023/src/Main.java
+++ COM4023/src/Main.java
@@ -1,20 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<addSong> songList = new ArrayList<>();
+        songList.add(new addSong("Saturday","Taylor Swift",193));
         //Print all the initial options and menu
         System.out.print("Welcome to the Music Player!\n");
-        System.out.print("You have 4 options to choose from: \n");
+        System.out.print("You have 5 options to choose from: \n");
         System.out.print("1. Add a new song to list\n");
         System.out.print("2. Remove a song from list\n");
         System.out.print("3. Show a list of all songs saved\n");
         System.out.print("4. Show a list of all songs ranked by plays\n");
+        System.out.print("5. Exit\n");
 
         //Record all responses
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Please enter the option you would like to use: ");
-        String option = scanner.nextLine();
+        while (true) {
+            System.out.print("Please enter the option you would like to use: ");
+            String option = scanner.nextLine();
+            switch (option) {
+                case "1":
+            }
+        }
 
 
 
---
+++ COM4023/src/addSong.java
@@ -0,0 +1,12 @@
+public class addSong {
+    private String name;
+    private String artistName;
+    private int playCount;
+
+
+    public addSong(String name, String artistName, int playCount) {
+        this.name = name;
+        this.artistName = artistName;
+        this.playCount = playCount;
+    }
+}
